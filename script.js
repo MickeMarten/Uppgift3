49fd2f562e8d4faed1b88f0d7054158fe1727474
MickeMarten <KarlMikael.Martensson@gmail.com>
2023-09-19 20:00:49 +0200

    Uppgift3

---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+function changeText() {
+    // change heading text when pressing button
+    var heading = document.getElementById('header1id');
+    heading.innerHTML = "Goodbye World!";
+    
+    
+    // Popup picture when pressing button
+    document.getElementById('nuke1')
+    .style.display = "block";
+    document.getElementById('button1id')
+
+    // Change text of button on click
+    buttonTextChange=document.getElementById("button1id")
+     buttonTextChange.innerHTML = "Repair world";
+
+
+
+}
+    
+function changeTextBack() {
+
+
+}
+
+
